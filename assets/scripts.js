ae1f37cb0bd05a9312e8a45d2be77c8840f58278
Souad-HB <souadsalahh@gmail.com>
2024-07-01 20:55:50 -0400

    added js file into the html file

---
+++ assets/scripts.js
@@ -0,0 +1,22 @@
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+ const shapes = ["Triange", "Square", "Circle", "Pentagon"]
+ console.log(shapes[0]);
+ for (let x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+ } */
